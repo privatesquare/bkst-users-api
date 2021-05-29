de136064a5e7be2796f91f8a8d0c56066d053bdd
Allan Tony Selvan <allan.tony.selvan@nl.abnamro.com>
2021-05-29 14:17:14 +0200

    Added table creation query

---
+++ data/users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `users` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(45) NOT NULL,
+    `last_name` varchar(45) NOT NULL,
+    `email` varchar(45) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `status` varchar(45) NOT NULL,
+    `date_created` datetime NOT NULL,
+    `date_updated` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email_UNIQUE` (`email`)
+);
